c25aea8c7dbf0cd7dd9d5723b5e121fafaabd180
Arjunr1234 <arjunreji1234@gmail.com>
2025-02-16 18:27:46 +0530

    changed the url

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000"; 
+//const BASE_URL = "http://localhost:5000"; 
+const BASE_URL = 'https://chatapp-backend-unji.onrender.com'
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
